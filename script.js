48124569e2ed4569858b3af708a0d130c173da85
gitviini <gvinicius105@gmail.com>
2024-07-31 22:20:41 -0300

    toggle hamburguer add

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const menu_container = document.querySelector('.menu-container')
 const main = document.querySelector('main')
 const toggle = document.querySelector('.toggle')
 
+ancors.forEach(ancors=>{
+    ancors.onclick=()=>{
+        toggle.children[0].classList.remove('click')
+        toggle.classList.remove('click')
+    }
+})
+
 main.onscroll = (e) =>{
     let y = Math.round(main.scrollTop / main.clientHeight)
     ancors.forEach(ancors=>ancors.classList.remove('click'))
